7cc5f9a1671a4f62b3c6529f4532fb313109ecc7
deadman613 <deepanshusoni613@gmail.com>
2026-09-24 14:25:37 +0530

    new changes in map

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -76,7 +76,7 @@ export default function Footer() {
             <div>
               <h4>Find Us</h4>
               <iframe
-                src="https://maps.google.com/maps?q=28.5415141,77.240201&z=16&output=embed"
+                src="https://maps.google.com/maps?q=28.5415141,77.240201&z=16&output=embedhttps://business.google.com/n/13139081987818720626/searchprofile?hl=en-GB"
                 width="100%"
                 height="180"
                 style={{ border: 0, borderRadius: '8px' }}
